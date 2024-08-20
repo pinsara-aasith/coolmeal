da76eaa7a7997c19791036f203d8ddc3d4578a26
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-20 13:39:09 +0530

    add loading page

--- coolmeal-app/lib/pages/LoadingPage.dart
+++ coolmeal-app/lib/pages/LoadingPage.dart
@@ -17,18 +17,10 @@ class _LoadingpageState extends State<Loadingpage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
-              'assets/images/logo.png',
+              'assets/images/Logo.png',
               height: 200,
             ),
             const SizedBox(height: 20),
-            const Text(
-              'COOLMEAL',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF68A47F), // Adjust color as needed
-              ),
-            ),
             const SizedBox(height: 10),
             const Text(
               'Welcome to a Healthier You!',
