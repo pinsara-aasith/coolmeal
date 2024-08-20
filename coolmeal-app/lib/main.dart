c081573a260a9bd5b660c62fbb86a9c8422697ff
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-20 14:34:29 +0530

    add starting page

--- coolmeal-app/lib/main.dart
+++ coolmeal-app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:coolmeal/pages/LoadingPage.dart';
+import 'package:coolmeal/pages/StartingPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Loadingpage(),
+      home: const Startingpage(),
     );
   }
 }
