707f7376fbceee3f9f1464b46273aa5702a444bd
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-21 22:25:09 +0530

    reafctor signup page code

---
+++ coolmeal-app/lib/widgets/PasswordRequirements.dart
@@ -0,0 +1,43 @@
+import 'package:coolmeal/widgets/RequirementItem.dart';
+import 'package:flutter/material.dart';
+
+class PasswordRequirements extends StatelessWidget {
+  final bool hasSpecialCharacter;
+  final bool hasNumber;
+  final bool hasUpperCase;
+  final bool isPasswordLengthValid;
+
+  PasswordRequirements({
+    required this.hasSpecialCharacter,
+    required this.hasNumber,
+    required this.hasUpperCase,
+    required this.isPasswordLengthValid,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Your password should contain'),
+        const SizedBox(height: 8),
+        RequirementItem(
+          text: '1 or more special characters',
+          isValid: hasSpecialCharacter,
+        ),
+        RequirementItem(
+          text: '1 or more numbers',
+          isValid: hasNumber,
+        ),
+        RequirementItem(
+          text: '1 upper case letter',
+          isValid: hasUpperCase,
+        ),
+        RequirementItem(
+          text: 'Between 8 and 20 characters',
+          isValid: isPasswordLengthValid,
+        ),
+      ],
+    );
+  }
+}
