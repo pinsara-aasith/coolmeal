3ea78559d7d3c9712d1bb6d0cccf2daff077ad66
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-24 18:15:35 +0530

    tested using pickle file is efficient way

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from model import train_knn_model
 
 app = FastAPI()
 
@@ -8,5 +9,17 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/prediction")
+def read_prediction():
+    input_data = [[1800, 154, 60, 23]]
+
+    return {"prediction": "This is a prediction"}
+
+
+# define data path
+data_path = "/notebooks/daily_menus.csv"
+# Train the model and save it
+model = train_knn_model(data_path)
+print("model trained successfully --------------------- ")
 # mention running port
 print("app is running on port 8000")
