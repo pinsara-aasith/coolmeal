1fce7039c491354756636c1a7aa66eabb81ed493
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-26 12:18:34 +0530

    make basic functionalities of chatbot

--- chatbot-service/server/app.py
+++ chatbot-service/server/app.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
-import createContext
+from createContext import createContext
 
 app = FastAPI()
+
+# create LLM chain ()
+chain = createContext()
+print("Load Chain Successfully --------------------- ")
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "Food chat bot Service"}
+
+
+@app.post("/chat")
+def chat(query: str):
+    response = chain.invoke(query)
+    return {"response": response}
 
 
 # mention running port
