0cb01ecd14dc535ae6c565988398a73b9f18a555
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-26 12:18:34 +0530

    make basic functionalities of chatbot

--- chatbot-service/server/createContext.py
+++ chatbot-service/server/createContext.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
 import os
 
 load_dotenv()
@@ -45,3 +49,32 @@ def createVectorDB():
     vector_db.save_local(vectordb_file_path)
 
     return vector_db
+
+
+def createContext():
+    vector_db = createVectorDB()
+    # create Retriver
+    retriever = vector_db.as_retriever(score_threshold=0.7)  ## Score Threshold
+    memory = ConversationBufferWindowMemory(k=5)
+    prompt_template = """
+    You are the specialized AI assistant for a food datas. Give most accurate answer to the following questions, Sometimes given context may be helpful to give response."
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    chain_type_kwargs = {"prompt": PROMPT}
+
+    chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        input_key="query",
+        memory=memory,
+        return_source_documents=False,
+        chain_type_kwargs=chain_type_kwargs,
+    )
+
+    return chain
