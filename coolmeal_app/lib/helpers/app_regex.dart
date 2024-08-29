8bfe0ef4540fdf2914c726e5159e78be3ac13995
Pinsara Aasith <aasithp@gmail.com>
2024-08-30 02:08:39 +0530

    change email regexp

--- coolmeal_app/lib/helpers/app_regex.dart
+++ coolmeal_app/lib/helpers/app_regex.dart
@@ -1,8 +1,10 @@
 class AppRegex {
   static bool isEmailValid(String email) {
-    return RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
         .hasMatch(email);
   }
+
+
 
   static bool isPasswordValid(String password) {
     return RegExp(r'^(?=.{6,})').hasMatch(password);
