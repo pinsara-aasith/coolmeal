d1d0f3bd7fbfba75e6195963852246be1b6870fd
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-20 10:13:25 +0530

    add getHistory and update chat api endpoints

--- chatbot-service/server/mongodb_helper.py
+++ chatbot-service/server/mongodb_helper.py
@@ -29,8 +29,29 @@ async def create_session(session_template: SessionTemplate):
     print("session_template -------------- ", session_template)
     try:
         result = session_collection.insert_one(
-            session_template.dict()
+            session_template
         )  # Convert Pydantic model to dict
         print(f"User inserted with ID: {result}")
     except OperationFailure as e:
         print(f"Insert operation failed: {e}")
+
+
+# Read (Find)
+async def find_session(user_id: str):
+    print("user_id -------------- ", user_id)
+    try:
+        result = session_collection.find({"user_id": user_id})
+        print(f"User found: {result}")
+    except OperationFailure as e:
+        print(f"Find operation failed: {e}")
+    return result
+
+
+# Get user for given session_id
+async def get_user(session_id: str):
+    print("session_id -------------- ", session_id)
+    try:
+        result = session_collection.find_one({"session_id": session_id})
+        print(f"User found: {result}")
+    except OperationFailure as e:
+        print(f"Find operation failed: {e}")
