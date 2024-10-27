75ae1180781d725ef4c6ed536d017b0d6d6269c5
Lakshan madhusanka <lakshanmadu1946130@gmail.com>
2024-10-27 21:29:24 +0530

    add count in dashboard

---
+++ coolmeal-admin/app/dashboard/dashboard/DashboardCard.tsx
@@ -0,0 +1,31 @@
+import { Box, Card, Flex, Text } from "@radix-ui/themes";
+import Image, { StaticImageData } from "next/image";
+
+interface Props {
+  count: number;
+  title: string;
+  img: StaticImageData;
+  color: string;
+}
+
+const DashboardCard = ({ count, title, img, color }: Props) => {
+  return (
+    <Box maxWidth="240px">
+      <Card className={color}>
+        <Flex gap="9" align="center" className="p-2">
+          <Flex className="mr-9" direction="column">
+            <Text as="div" size="7" weight="bold">
+              {count}
+            </Text>
+            <Text as="div" size="4" color="gray">
+              {title}
+            </Text>
+          </Flex>
+          <Image src={img} alt="" width={40} />
+        </Flex>
+      </Card>
+    </Box>
+  );
+};
+
+export default DashboardCard;
--- coolmeal-admin/app/dashboard/page.tsx
+++ coolmeal-admin/app/dashboard/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Example from "./dashboard/Charts";
+import { Flex } from "@radix-ui/themes";
+
+import User from "@/public/user.png";
+import DashboardCard from "./dashboard/DashboardCard";
+import Meal from "@/public/meal.png";
 
 const Dashboard = () => {
-  return <div>Todo: Show analytics</div>;
+  return (
+    <div className="m-3">
+      <Flex gap="5">
+        <DashboardCard
+          count={500}
+          title="User"
+          img={User}
+          color="bg-green-400"
+        />
+        <DashboardCard
+          count={500}
+          title="Meal Plans"
+          img={Meal}
+          color="bg-green-800"
+        />
+      </Flex>
+      <Example />
+    </div>
+  );
 };
 
 export default Dashboard;
