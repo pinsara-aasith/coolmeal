249ddbee21922f0e090f47b0dcd81b7f62386992
Lakshan madhusanka <lakshanmadu1946130@gmail.com>
2024-10-29 23:55:22 +0530

    create admin dashboard

--- coolmeal-admin/app/dashboard/page.tsx
+++ coolmeal-admin/app/dashboard/page.tsx
@@ -1,25 +1,49 @@
+"use client";
 import Meal from "@/public/meal.png";
 import User from "@/public/user.png";
+import Ingredient from "@/public/ingredient.png";
 import { Flex } from "@radix-ui/themes";
-import Charts from "./dashboard/Charts";
 import DashboardCard from "./dashboard/DashboardCard";
 import DashboardGrid from "./dashboard/DashboardGrid";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [userCount, setUserCount] = useState();
+  console.log(userCount);
+
+  useEffect(() => {
+    const fetchUserCount = async () => {
+      try {
+        const response = await axios.get("/api/dashboard/users/count");
+        setUserCount(response.data.count);
+      } catch (error) {
+        console.error("Error fetching user count:", error);
+      }
+    };
+
+    fetchUserCount();
+  }, []);
   return (
     <div className="m-3">
       <Flex gap="5">
         <DashboardCard
-          count={500}
+          count={userCount!}
           title="User"
           img={User}
           color="bg-green-400"
         />
         <DashboardCard
-          count={500}
+          count={200000}
           title="Meal Plans"
           img={Meal}
           color="bg-blue-400"
+        />
+        <DashboardCard
+          count={300}
+          title="Ingredients"
+          img={Ingredient}
+          color="bg-red-400"
         />
       </Flex>
       <DashboardGrid />
